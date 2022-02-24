7a552db07314d7c6927c3c8d3635e08599c94011
G28XYZ <g28xyz@ya.ru>
2022-02-24 10:19:56 +0700

    run eject

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,22 @@ function App() {
 
   const level = new Level(GAME_LEVELS[0], levelChars);
 
+  function trackKeys(keys) {
+    let down = Object.create(null);
+    function track(event) {
+      if (keys.includes(event.key)) {
+        down[event.key] = event.type === "keydown";
+        event.preventDefault();
+      }
+    }
+    window.addEventListener("keydown", track);
+    window.addEventListener("keyup", track);
+    return down;
+  }
+  const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+  function handleMovePlayer() {}
+
   return (
     <div className="game">
       <DrawGrid level={level} />
